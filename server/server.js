ea49eeca8a6010fb4ac9595a7a7413031b36c670
Vinay Kushwaha <iamvinayvk@gmail.com>
2024-11-30 19:55:22 +0530

    Fixed the cors issue

--- server/server.js
+++ server/server.js
@@ -1,72 +1,46 @@
-const express = require('express');
+const express = require("express");
 const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
-
-
 app.listen(PORT, function () {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 let clients = {};
 
 function sendMessage(message) {
-    console.log(`Sending message: ${message}`);
-    // console.log(clients.length);
-    // send message to clients
-    for (const clientId in clients) {
-        clients[clientId].write(`data: ${message}\n\n`);
-    }
+  console.log(`Sending message: ${message}`);
+  for (const clientId in clients) {
+    clients[clientId].write(`data: ${message}\n\n`);
+  }
 }
 
+app.get("/events/sse", function (req, res) {
+  res.setHeader("Content-Type", "text/event-stream");
+  const eventSource = req.get("Accept").includes("text/event-stream")
+    ? res
+    : null;
 
-app.get('/events/sse', function (req, res) {
-
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-
-    const eventSource = req.get('Accept').includes('text/event-stream') ? res : null;
-
-    if (eventSource) {
-        const clientId = uuidv4();
-        clients[clientId] = eventSource;
-        console.log(`New client connected: ${clientId}`);
-        console.log(clients.length);
-        eventSource.write(`data: Hello, client ${clientId}\n\n`);
-        // const intervalId = setInterval(() => {
-        //     eventSource.write(`data: Hello, Client id:${clientId} Server Time: ${new Date().toISOString()}\n\n`);
-        // }, 1000);
-
-        req.on('close', () => {
-            delete clients[clientId];
-            // clearInterval(intervalId);
-        });
-    } else {
-        res.status(406).send('Not Acceptable');
-    }
+  if (eventSource) {
+    const clientId = uuidv4();
+    clients[clientId] = eventSource;
+    console.log(`New client connected: ${clientId}`);
+    eventSource.write(`data: Hello, client ${clientId}\n\n`);
+    req.on("close", () => {
+      delete clients[clientId];
+    });
+  } else {
+    res.status(406).send("Not Acceptable");
+  }
 });
 
-app.options('/send-message', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.sendStatus(200);
+app.post("/send-message", function (req, res) {
+  const message = req.body.text;
+  sendMessage(message);
+  res.send({ message: "Message sent successfully" });
 });
-
-app.post('/send-message', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-
-    const message = req.body.text;
-    console.log(message);
-    sendMessage(message);
-    res.send({ message: 'Message sent successfully' });
-
-})
